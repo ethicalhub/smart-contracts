8cfea7fef7419a3b1c490359eb37cd919f0b1c81
ethicalhub <yasiransari.elith@gmail.com>
2022-04-23 17:00:48 +0530

    First Contract Ecommerce

---
+++ ecommerce01.sol
@@ -0,0 +1,59 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.0 <0.9.0;
+
+
+contract ECommerce{
+    struct Product{
+        string title;
+        string desc;
+        uint price;
+        uint productID;
+        bool delivered;
+        address payable seller;
+        address buyer;
+    }
+
+    uint count = 1;
+    Product[] public products;
+
+    event registered(string title, uint productID, address payable seller);
+    event bought(uint productID, address Buyers);
+    event delivered(uint productID);
+
+    function registerProduct(
+        string memory _title,
+        string memory _desc,
+        uint _price
+        ) public{
+            require(_price >0 , "Price must be greater than 0, not free");
+
+            Product memory newProduct;
+            newProduct.title = _title;
+            newProduct.desc = _desc;
+            newProduct.price = _price * 10**18;
+            newProduct.productID = count;
+            newProduct.seller = payable(msg.sender);
+            products.push(newProduct);
+            count++;
+            emit registered(_title, newProduct.productID, payable(msg.sender));
+    }
+
+    function buy(uint _productID) payable public{
+        require(products[_productID-1].price == msg.value, "Send the exact price only");
+        require(products[_productID-1].seller != msg.sender, "Sender cannot be the buyer");
+
+        products[_productID-1].buyer = msg.sender;
+        emit bought(_productID, msg.sender );
+    }
+
+    function delivery(uint _productID) public{
+        require(products[_productID-1].buyer == msg.sender , "Only Buyers are allowed to verify delivery");
+        products[_productID-1].delivered = true;
+        products[_productID-1].seller.transfer(products[_productID-1].price);
+        emit delivered(_productID);
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+}
